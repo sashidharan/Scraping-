12602068da9853d36a28897d4c2440f9fed17e64
SM <swadesSNV@gmail.com>
2017-07-20 00:58:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+  var request=require('request');
+  var cheerio=require('cheerio');
+  var express=require('express');
+  var fs=require('fs');
+  var app=express();
+  request('http://directory.stokesentinel.co.uk/search/stoke-on-trent%2Cstaffordshire/temple', function (error, response, html) {
+    if (!error && response.statusCode == 200) {
+      // console.log(html);
+      var $=cheerio.load(html);
+      var json=[];
+      // let i=0;
+      $('h2.ser-title').each(function(){
+      	var result = $(this);
+        var shop={title:result.text()};
+        json.push(shop);
+       //  i++;
+       // console.log(i);
+      	// console.log(result.text());
+      });
+      console.log(json)
+
+    }
+    else{
+    	console.log(error);
+    }
+  });
+
+  app.listen(4000,function(){
+  	console.log("Done!");
+  });
